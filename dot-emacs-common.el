e040ca149b7ad5003c8ec396197e7989e7cdf7bb
Keunwoo Lee <k@keunwoo.com>
2022-11-13 11:46:32 -0800

    [lsp] enable go, disable which-key

--- dot-emacs-common.el
+++ dot-emacs-common.el
@@ -369,8 +369,10 @@
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (type-mode . lsp)
+         (go-mode . lsp)
          ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
+         ;;(lsp-mode . lsp-enable-which-key-integration)
+         )
   :commands lsp)
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package helm-lsp :commands helm-lsp-workspace-symbol)
