7afb76333557a6f9cc72b2a5d66c14e5996ac04e
Keunwoo Lee <keunwoo@airtable.com>
2024-08-29 11:30:05 -0700

    rm json-mode from 'auto-mode-alist

--- dot-emacs.el
+++ dot-emacs.el
@@ -348,11 +348,11 @@
 ;; (add-to-list 'auto-mode-alist '("\\.json$" . fundamental-mode))
 ;;
 ;; no, use json-mode
-(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
-(add-hook 'json-mode-hook
-          (lambda ()
-            (make-local-variable 'js-indent-level)
-                        (setq js-indent-level 2)))
+;;(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+;;(add-hook 'json-mode-hook
+;;          (lambda ()
+;;            (make-local-variable 'js-indent-level)
+;;                        (setq js-indent-level 2)))
 
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
 
